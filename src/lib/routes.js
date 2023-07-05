242f789e38788739a8d8d8cf2ab0b9d87724b756
Robert Kexim <136731188+NotCoderBoy@users.noreply.github.com>
2023-07-05 14:00:29 +0000

    Update 7 files

--- src/lib/routes.js
+++ src/lib/routes.js
@@ -2,15 +2,19 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "pages/login/Login";
 import Register from "pages/register/Register";
 import Home from "pages/home/Home";
+import Dashboard from "pages/dashboard/Dashboard";
+
 export const paths = {
   // pages
   ROOT: "/",
   LOGIN: "/login",
   REGISTER: "/register",
+  DASHBOARD: "/dashboard",
 };
 
 export const router = createBrowserRouter([
   { path: paths.ROOT, element: <Home /> },
   { path: paths.LOGIN, element: <Login /> },
   { path: paths.REGISTER, element: <Register /> },
+  { path: paths.DASHBOARD, element: <Dashboard /> },
 ]);
---
+++ src/pages/dashboard/Dashboard.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+
+// components
+import Sidebar from "../../components/Sidebar";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+import Preloader from "../../components/Preloader";
+
+export default function Dashboard() {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoaded(true), 1000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <>
+      <Preloader show={loaded ? false : true} />
+      <Sidebar />
+
+      <main className="content">
+        <Navbar />
+        <div>Dashboard</div>
+        <Footer />
+      </main>
+    </>
+  );
+}
