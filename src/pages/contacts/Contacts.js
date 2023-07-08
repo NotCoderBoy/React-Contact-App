a5bd4e0a1da4795becf55e2921ecd912be744229
Robert Kexim <136731188+NotCoderBoy@users.noreply.github.com>
2023-07-08 06:56:06 +0000

    Update 7 files

--- src/pages/contacts/Contacts.js
+++ src/pages/contacts/Contacts.js
@@ -15,7 +15,13 @@ import {
   Breadcrumb,
   InputGroup,
   Dropdown,
+  Card,
 } from "@themesberg/react-bootstrap";
+import Table from "react-bootstrap-table-next";
+import Pagination from "react-bootstrap-table2-paginator";
+import * as Paginator from "react-bootstrap-table2-paginator";
+import ToolkitProvider, { Search } from "react-bootstrap-table2-toolkit";
+import entries from "../../jobs";
 
 import { ContactTable } from "../../widgets/Tables";
 
@@ -32,7 +38,132 @@ export default function Dashboard() {
     const timer = setTimeout(() => setLoaded(true), 1000);
     return () => clearTimeout(timer);
   }, []);
+  const columns = [
+    { dataField: "id", text: "ID", hidden: true },
+    { dataField: "name", text: "Name" },
+    { dataField: "position", text: "Position" },
+    { dataField: "office", text: "Office" },
+    { dataField: "age", text: "Age" },
+    { dataField: "startDate", text: "Start date" },
+    {
+      dataField: "salary",
+      text: "Salary",
+      formatter: (cell) => <span>${cell}</span>,
+    },
+  ];
 
+  const customTotal = (from, to, size) => (
+    <div>
+      Showing {from} to {to} of {size} entries
+    </div>
+  );
+
+  const customSizePerPage = (props) => {
+    const { options, currentSizePerPage, onSizePerPageChange } = props;
+
+    const onPageChange = (e) => {
+      const page = e.target.value;
+      onSizePerPageChange(page);
+    };
+
+    return (
+      <Row>
+        <Col xs="auto">
+          <Form.Select
+            value={currentSizePerPage}
+            onChange={onPageChange}
+            className="pe-5"
+          >
+            {options.map((o) => (
+              <option key={o.page} value={o.page}>
+                {o.text}
+              </option>
+            ))}
+          </Form.Select>
+        </Col>
+        <Col xs="auto" className="d-flex align-items-center">
+          entries per page
+        </Col>
+      </Row>
+    );
+  };
+
+  render(
+    <ToolkitProvider
+      keyField="id"
+      search={true}
+      columns={columns}
+      data={entries}
+      wrapperClasses="table-responsive"
+    >
+      {({ baseProps, searchProps }) => (
+        <Paginator.PaginationProvider
+          pagination={Pagination({
+            custom: true,
+            showTotal: true,
+            alwaysShowAllBtns: true,
+            totalSize: entries.length,
+            paginationTotalRenderer: customTotal,
+            sizePerPageRenderer: customSizePerPage,
+          })}
+        >
+          {({ paginationProps, paginationTableProps }) => (
+            <Card>
+              <div className="table-responsive py-2">
+                <Card.Header>
+                  <Row>
+                    <Col xs={12} md={6} className="py-1">
+                      <Paginator.SizePerPageDropdownStandalone
+                        {...paginationProps}
+                      />
+                    </Col>
+                    <Col
+                      xs={12}
+                      md={6}
+                      className="d-flex justify-content-md-end py-1"
+                    >
+                      <Search.SearchBar {...searchProps} />
+                    </Col>
+                  </Row>
+                </Card.Header>
+
+                <Table
+                  {...baseProps}
+                  {...paginationTableProps}
+                  bodyClasses="border-0"
+                  rowClasses="border-bottom"
+                  classes="table-flush dataTable-table"
+                />
+
+                <Card.Footer>
+                  <Row>
+                    <Col
+                      xs={12}
+                      md={6}
+                      className="d-flex align-items-center py-1"
+                    >
+                      <Paginator.PaginationTotalStandalone
+                        {...paginationProps}
+                      />
+                    </Col>
+                    <Col
+                      xs={12}
+                      md={6}
+                      className="d-flex justify-content-md-end align-items-center mb-0 py-1"
+                    >
+                      <Paginator.PaginationListStandalone
+                        {...paginationProps}
+                      />
+                    </Col>
+                  </Row>
+                </Card.Footer>
+              </div>
+            </Card>
+          )}
+        </Paginator.PaginationProvider>
+      )}
+    </ToolkitProvider>
+  );
   return (
     <>
       <Preloader show={loaded ? false : true} />
