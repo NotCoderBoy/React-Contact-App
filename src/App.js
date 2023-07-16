416ee3602004504175017a1a4d87bae1748fce22
Robert Kexim <robertkexim1998@gmail.com>
2023-07-16 16:20:00 +0000

    Update 4 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "lib/routes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App() {
   return (
     <div className="App">
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       <RouterProvider router={router} />
     </div>
   );
